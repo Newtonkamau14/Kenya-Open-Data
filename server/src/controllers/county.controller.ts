bed86286bce50001a366124e0cc022c412d2af85
Newtonkamau14 <newtonkamau14@gmail.com>
2024-06-18 23:52:09 +0300

    add different methods for the different endpoints

---
+++ server/src/controllers/county.controller.ts
@@ -0,0 +1,301 @@
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import * as fs from "fs";
+import * as path from "path";
+import { CountyRepository } from "../repository/county.repository";
+import { ICounty } from "../models/county";
+import { logger } from "../util/util";
+
+export class CountyController {
+  private static countyRepository = new CountyRepository();
+
+  static async addAll(req: Request, res: Response): Promise<void> {
+    try {
+      const filePath = path.join(
+        __dirname,
+        "../../modified_combined_county_data.json"
+      );
+      const counties = (await CountyController.readJSONFile(
+        filePath
+      )) as ICounty[];
+      await CountyController.countyRepository.addAllCountyData(counties);
+      res.status(200).json({ message: "Data successfully inserted" });
+    } catch (error) {
+      logger.error("Failed to insert data", error);
+      res.status(500).json({ message: "Failed to insert data", error });
+    }
+  }
+
+  // Function to read JSON file
+  private static async readJSONFile(filePath: string): Promise<ICounty[]> {
+    try {
+      const data = fs.readFileSync(filePath, "utf-8");
+      const parsedData = JSON.parse(data);
+
+      // Ensure parsedData is an array
+      if (!Array.isArray(parsedData)) {
+        throw new Error("Parsed data is not an array");
+      }
+
+      // Convert to the correct format if necessary
+      const counties: ICounty[] = parsedData.map(
+        (county: any) =>
+          ({
+            id: uuidv4(),
+            countyCode: county["County Code"],
+            countyName: county["County Name"],
+            size: parseFloat(county["Size(km2)"]),
+            population: parseInt(county["Population(KPHC 2019)"], 10),
+            populationDensity: parseInt(county["Population Density(km2)"], 10),
+            numberOfHouseholds: parseInt(county["Number of Households"], 10),
+            averageHouseholdSize: parseInt(
+              county["Average Household size"],
+              10
+            ),
+            sexRatio: parseInt(
+              county["Sex Ratio(No of Males per 100 females)"],
+              10
+            ),
+            capital: county["Capital"],
+            governor: county["Governor"],
+            deputyGovernor: county["Deputy Governor"],
+            womenRepresentative: county["Women Representative"],
+            mainEconomicActivities: Array.isArray(
+              county["Main Economic Activities"]
+            )
+              ? county["Main Economic Activities"]
+              : [],
+            constituencies: Array.isArray(county["Constituencies"])
+              ? county["Constituencies"].map((constituency: any) => ({
+                  constituencyCode: constituency["Constituency Code"],
+                  constituencyName: constituency["Constituency Name"],
+                  registeredVoters: parseInt(
+                    constituency["Registered Voters"],
+                    10
+                  ),
+                }))
+              : [],
+          } as ICounty)
+      ); // Assert that the created object is of type ICounty
+
+      return counties;
+    } catch (error) {
+      console.error("Error reading or parsing JSON file:", error);
+      throw error;
+    }
+  }
+
+  static async getAllCounties(req: Request, res: Response): Promise<void> {
+    try {
+      const counties = await CountyController.countyRepository.getAllCounties();
+      if (counties.length === 0) {
+        res.status(204).json({ message: "No counties found" });
+      } else {
+        res.status(200).json(counties);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountyByCountyCode(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const { countyCode } = req.params;
+    try {
+      const county =
+        await CountyController.countyRepository.getCountyByCountyCode(
+          countyCode
+        );
+
+      if (!county) {
+        res.status(404).json({ message: "No county found" });
+      } else {
+        res.status(200).json(county);
+      }
+    } catch (error) {
+      logger.error("Error in getting county", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountiesBySize(req: Request, res: Response): Promise<void> {
+    const { order } = req.query;
+    console.log("Received request with size order:", order); // Log the incoming request
+
+    try {
+      if (order !== "ASC" && order !== "DESC") {
+        res.status(400).json({ message: "Not a valid size order" });
+      }
+
+      const counties =
+        await CountyController.countyRepository.getCountiesBySize(
+          order as "ASC" | "DESC"
+        );
+
+      if (!counties || counties.length === 0) {
+        res.status(204).json({ message: "No counties found" });
+      }
+
+      res.status(200).json(counties);
+    } catch (error) {
+      logger.error("Error in getting counties by size", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountiesPopulation(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const counties =
+        await CountyController.countyRepository.getCountiesPopulation();
+
+      if (!counties) {
+        res.status(404).json({ message: "Counties population not found" });
+      }
+      if (counties.length === 0) {
+        res.status(204).json({ message: "No counties population" });
+      } else {
+        res.status(200).json(counties);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties population", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountiesPopulationDensity(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const counties =
+        await CountyController.countyRepository.getCountiesPopulationDensity();
+
+      if (!counties) {
+        res
+          .status(404)
+          .json({ message: "Counties population density not found" });
+      }
+      if (counties.length === 0) {
+        res.status(204).json({ message: "No counties population density" });
+      } else {
+        res.status(200).json(counties);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties population density", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountiesHousehold(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const counties =
+        await CountyController.countyRepository.getCountiesHousehold();
+
+      if (!counties) {
+        res
+          .status(404)
+          .json({ message: "Counties population household not found" });
+      }
+      if (counties.length === 0) {
+        res.status(204).json({ message: "No counties household" });
+      } else {
+        res.status(200).json(counties);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties household", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getCountiesCapital(req: Request, res: Response): Promise<void> {
+    try {
+      const counties =
+        await CountyController.countyRepository.getCountiesCapital();
+
+      if (!counties) {
+        res.status(404).json({ message: "Counties capital not found" });
+      }
+      if (counties.length === 0) {
+        res.status(204).json({ message: "No counties capital" });
+      } else {
+        res.status(200).json(counties);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties capital", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getConstituenciesByCounty(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const constituencies =
+        await CountyController.countyRepository.getConstituenciesByCounty();
+
+      if (!constituencies) {
+        res.status(404).json({ message: "No constituencies" });
+      }
+      if (constituencies.length === 0) {
+        res.status(204).json({ message: "No constituencies found" });
+      } else {
+        res.status(200).json(constituencies);
+      }
+    } catch (error) {
+      logger.error("Error in getting counties by size", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getConstituencies(req: Request, res: Response): Promise<void> {
+    try {
+      const constituencies =
+        await CountyController.countyRepository.getConstituencies();
+
+      if (!constituencies) {
+        res.status(404).json({ message: "No constituencies" });
+      }
+      if (constituencies.length === 0) {
+        res.status(204).json({ message: "No constituencies found" });
+      } else {
+        res.status(200).json(constituencies);
+      }
+    } catch (error) {
+      logger.error("Error in getting constituencies", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getConstituencyByConstituencyCode(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const { constituencyCode } = req.params;
+
+      const constituency =
+        await CountyController.countyRepository.getConstituencyByConstituencyCode(
+          constituencyCode
+        );
+
+      if (!constituency) {
+        res.status(404).json({ message: "No constituencies" });
+      } else {
+        res.status(200).json(constituency);
+      }
+    } catch (error) {
+      logger.error("Error in getting constituency by code", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
---
+++ server/src/repository/county.repository.ts
@@ -0,0 +1,346 @@
+import { ResultSetHeader } from "mysql2";
+import { connection } from "../config/database";
+import { v4 as uuidv4 } from "uuid";
+import { ICounty, IConstituency } from "../models/county";
+
+export class CountyRepository {
+  async addAllCountyData(counties: ICounty[]): Promise<void> {
+    try {
+      for (const county of counties) {
+        const countyId = uuidv4();
+        console.log("Inserting county:", JSON.stringify(county, null, 2)); // Log county data for debugging
+
+        // Insert county data
+        await connection.execute<ResultSetHeader>(
+          `INSERT INTO counties (id, countyCode, countyName, size, population , populationDensity, numberOfHouseholds, averageHouseholdSize, sexRatio, capital, governor, deputyGovernor, womenRepresentative) 
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+          [
+            countyId,
+            county.countyCode,
+            county.countyName,
+            county.size,
+            county.population,
+            county.populationDensity,
+            county.numberOfHouseholds,
+            county.averageHouseholdSize,
+            county.sexRatio,
+            county.capital,
+            county.governor,
+            county.deputyGovernor,
+            county.womenRepresentative,
+          ]
+        );
+
+        // Ensure mainEconomicActivities is an array before iterating
+        if (Array.isArray(county.mainEconomicActivities)) {
+          for (const activity of county.mainEconomicActivities) {
+            await connection.execute<ResultSetHeader>(
+              `INSERT INTO economic_activities (countyId, activity) VALUES (?, ?)`,
+              [countyId, activity]
+            );
+          }
+        } else {
+          console.warn(
+            `mainEconomicActivities for county ${county.countyCode} is not an array.`
+          );
+        }
+
+        // Ensure constituencies is an array before iterating
+        if (Array.isArray(county.constituencies)) {
+          for (const constituency of county.constituencies) {
+            await connection.execute<ResultSetHeader>(
+              `INSERT INTO constituencies (id, countyId, constituencyCode, constituencyName, registeredVoters) 
+              VALUES (?, ?, ?, ?, ?)`,
+              [
+                uuidv4(),
+                countyId,
+                constituency.constituencyCode,
+                constituency.constituencyName,
+                constituency.registeredVoters,
+              ]
+            );
+          }
+        } else {
+          console.warn(
+            `Constituencies for county ${county.countyCode} is not an array.`
+          );
+        }
+      }
+    } catch (err) {
+      console.error("Error inserting data:", err);
+      throw err;
+    } finally {
+      await connection.end();
+    }
+  }
+
+  getAllCounties(): Promise<ICounty[]> {
+    return new Promise((resolve, reject) => {
+      connection.query<ICounty[]>(
+        `SELECT 
+            counties.countyCode,
+            counties.countyName,
+            counties.size,
+            counties.population,
+            counties.populationDensity,
+            counties.numberOfHouseholds,
+            counties.averageHouseholdSize,
+            counties.sexRatio,
+            counties.capital,
+            counties.governor,
+            counties.deputyGovernor,
+            counties.womenRepresentative,
+            JSON_ARRAYAGG(JSON_OBJECT('Constituency Code', c.constituencyCode, 'Constituency Name', c.constituencyName)) AS constituencies
+          FROM counties
+          INNER JOIN (
+            SELECT *
+            FROM constituencies
+            ORDER BY constituencyCode ASC
+          ) AS c ON counties.id = c.countyId
+          GROUP BY counties.countyCode, counties.countyName
+          ORDER BY counties.countyCode;`,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  }
+  getCountyByCountyCode(countyCode: string): Promise<ICounty | undefined> {
+    return new Promise((resolve, reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+          counties.countyCode,
+          counties.countyName,
+          counties.size,
+          counties.population,
+          counties.populationDensity,
+          counties.numberOfHouseholds,
+          counties.averageHouseholdSize,
+          counties.sexRatio,
+          counties.capital,
+          counties.governor,
+          counties.deputyGovernor,
+          counties.womenRepresentative,
+          JSON_ARRAYAGG(JSON_OBJECT('Constituency Code', constituencies.constituencyCode, 'Constituency Name', constituencies.constituencyName)) AS constituencies
+        FROM counties
+        INNER JOIN constituencies ON
+            counties.id = constituencies.countyId
+        WHERE counties.countyCode = ?
+        GROUP BY
+          counties.countyCode,
+          counties.countyName,
+          counties.size,
+          counties.population,
+          counties.populationDensity,
+          counties.numberOfHouseholds,
+          counties.averageHouseholdSize,
+          counties.sexRatio,
+          counties.capital,
+          counties.governor,
+          counties.deputyGovernor,
+          counties.womenRepresentative
+        ORDER BY counties.countyCode    
+        `,
+        [countyCode],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results?.[0]);
+          }
+        }
+      );
+    });
+  }
+
+  getCountiesBySize(order: "ASC" | "DESC"): Promise<ICounty[]> {
+    return new Promise((resolve, reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+            countyCode,
+            countyName,
+            size
+         FROM
+          counties
+         ORDER BY
+            size ${order}
+        `,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  }
+
+  getCountiesPopulation(): Promise<ICounty[]> {
+    return new Promise((resolve,reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+            countyCode,
+            countyName,
+            population
+         FROM
+            counties   
+        `,
+        (err,results) => {
+          if(err){
+            reject(err)
+          }
+          else {
+            resolve(results)
+          }
+        }
+      )
+    })
+  }
+
+  getCountiesPopulationDensity(): Promise<ICounty[]> {
+    return new Promise((resolve,reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+            countyCode,
+            countyName,
+            populationDensity
+         FROM
+            counties   
+        `,(err,results) => {
+          if(err) {
+            reject(err)
+          }
+          else {
+            resolve(results)
+          }
+        }
+      )
+    })
+  }
+
+  getCountiesHousehold(): Promise<ICounty[]> {
+    return new Promise((resolve,reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+            countyCode,
+            countyName,
+            numberOfHouseholds,
+            averageHouseholdSize
+         FROM
+            counties   
+        `,(err,results) => {
+          if(err){
+            reject(err)
+          }
+          else {
+            resolve(results)
+          }
+        }
+      )
+    })
+  }
+
+
+  getCountiesRegisteredVoters(): Promise<ICounty[]> {
+    return new Promise((resolve,reject) => {
+      connection.query
+    })
+  }
+
+  getCountiesCapital(): Promise<ICounty[]> {
+    return new Promise((resolve,reject) => {
+      connection.query<ICounty[]>(
+        `SELECT
+            countyCode,
+            countyName,
+            capital
+         FROM
+            counties   
+        `,(err,results) => {
+          if(err) {
+            reject(err)
+          }
+          else {
+            resolve(results)
+          }
+        }
+      )
+    })
+  }
+
+  getConstituenciesByCounty(): Promise<IConstituency[]> {
+    return new Promise((resolve, reject) => {
+      connection.query<IConstituency[]>(
+        `SELECT 
+            counties.countyName,
+            JSON_ARRAYAGG(JSON_OBJECT('Constituency Code', constituencies.constituencyCode, 'Constituency Name', constituencies.constituencyName)) AS constituencies
+         FROM
+            constituencies
+         INNER JOIN counties ON
+            constituencies.countyId = counties.id
+         GROUP BY
+            counties.countyName
+        `,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  }
+
+  getConstituencies(): Promise<IConstituency[]>{
+    return new Promise((resolve,reject) => {
+      connection.query<IConstituency[]>(
+        `SELECT
+            constituencyCode,
+            constituencyName,
+            registeredVoters
+         FROM
+            constituencies
+         ORDER BY constituencyCode ASC  
+        `,(err,results) => {
+          if(err) {
+            reject(err)
+          }
+          else {
+            resolve(results)
+          }
+        } 
+      )
+    })
+  }
+
+
+  getConstituencyByConstituencyCode(constituencyCode: string): Promise<IConstituency | undefined> {
+    return new Promise((resolve,reject) => {
+      connection.query<IConstituency[]>(
+        `SELECT
+            constituencyCode,
+            constituencyName,
+            registeredVoters
+         FROM
+            constituencies 
+         WHERE constituencyCode = ? 
+        `,
+        [constituencyCode],
+        (err,results) => {
+          if(err) {
+            reject(err)
+          }
+          else {
+            resolve(results?.[0])
+          }
+        }
+      )
+    })
+  }
+}
