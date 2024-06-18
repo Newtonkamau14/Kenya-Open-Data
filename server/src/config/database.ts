79994ddfc5645b32f1473bf85227a4739207d8e2
Newtonkamau14 <newtonkamau14@gmail.com>
2024-06-18 23:45:24 +0300

    remove database connection function

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,19 +2,11 @@ import mysql, { ConnectionOptions } from "mysql2";
 
 const access: ConnectionOptions = {
   host: process.env.DATABASE_HOST,
-  user: process.env.DATABSE_USER,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
 };
 
 const connection = mysql.createConnection(access);
-
-/* connection.connect(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Database connected");
-  }
-}); */
 
 export { connection };
