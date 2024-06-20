8d01bf7ab187fd0bd09712bd9105f5d644076c30
Newtonkamau14 <newtonkamau14@gmail.com>
2024-06-20 12:27:48 +0300

    add express-session and express-mysql-session configurations

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,40 @@ import YAML from "yamljs";
 import swaggerUI from "swagger-ui-express";
 import path from "path";
 import cors from "cors";
+import * as session from "express-session";
+import MySQLStore, { Options } from "express-mysql-session";
 import { normalizePort } from "./util/util";
 import { connection } from "./config/database";
 import router from "./routes/index";
+
+
 const PORT = normalizePort(process.env.PORT || "3000");
 const app: Application = express();
+const MySQLStoreInstance = MySQLStore(session);
+
+const options: Options = {
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  clearExpired: true,
+  checkExpirationInterval: 900000,
+  expiration: 86400000,
+  createDatabaseTable: true,
+  endConnectionOnClose: true,
+  charset: 'utf8mb4_bin',
+	schema: {
+		tableName: 'sessions',
+		columnNames: {
+			session_id: 'session_id',
+			expires: 'expires',
+			data: 'data'
+		}
+	}
+};
+
+const sessionStore = new MySQLStoreInstance(options);
 
 if (PORT === false) {
   console.error("Invalid port. Server not starting.");
@@ -25,9 +54,22 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(router)
+// app.use(cors());
+app.use(session.default({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 259200000,
+    httpOnly: true,
+    sameSite: 'none'
+  }
+}))
 
+
+app.use(router);
 
 
 //Start server
@@ -49,3 +91,5 @@ try {
 }
 
 export { app };
+
+
