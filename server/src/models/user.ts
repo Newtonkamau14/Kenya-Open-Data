7b8d6e65b20c15e21761c9c5c89bcdcf4452a28a
Newtonkamau14 <newtonkamau14@gmail.com>
2024-06-20 12:32:45 +0300

    add user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,9 @@
+import { RowDataPacket } from "mysql2";
+
+export interface IUser extends RowDataPacket {
+  id: string;
+  username?: string;
+  email: string;
+  password: string;
+  phonenumber?: string;
+}
