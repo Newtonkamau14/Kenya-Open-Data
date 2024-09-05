e95bc80a98f5005f4707e5d1918faea9598dddf8
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-05 22:44:58 +0300

    refactor from countyRouter to apiRouter

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import countyRouter from "./county.router";
+import apiRouter from "./api.router";
 import authRouter from "./auth.router";
 const router = Router();
 
-router.use("/", countyRouter);
+router.use("/", apiRouter);
 router.use("/auth", authRouter);
 
 export default router;
