cd6e824aca344838e3eed839ffa527e29507f588
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-13 12:22:07 +0300

    add ssl

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,10 +1,16 @@
 import mysql, { ConnectionOptions } from "mysql2";
 
+
 const access: ConnectionOptions = {
+  port: process.env.DATABASE_PORT,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  ssl: {
+    ca: process.env.CA_CERT,
+    rejectUnauthorized: true
+  }
 };
 
 const connection = mysql.createConnection(access);
