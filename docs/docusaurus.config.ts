c2b83740b4c20e7c0122a1ef05e14b5e33c1c0ce
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-25 14:19:09 +0300

    style: format code

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,40 +1,43 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
 
 const config: Config = {
-  title: 'Kenya Open Data',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "Kenya Open Data",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://google.com',
+  url: "https://newtonkamau14.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/Kenya-Open-Data/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Newtonkamau14', // Usually your GitHub org/user name.
-  projectName: 'Kenya-Open-Data', // Usually your repo name.
+  organizationName: "Newtonkamau14", // Usually your GitHub org/user name.
+  projectName: "Kenya-Open-Data", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
+  deploymentBranch: "gh-pages",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
@@ -44,7 +47,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -52,44 +55,44 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'Kenya Open Data',
+      title: "Kenya Open Data",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         {
-          href: 'https://github.com/Newtonkamau14/Kenya-Open-Data',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/Newtonkamau14/Kenya-Open-Data",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://google.com',
-          label: 'Login/Sign Up',
-          position: 'right'
-        }
+          href: "https://google.com",
+          label: "Login/Sign Up",
+          position: "right",
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
-        }
+        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Kenya Open Data.`,
     },
