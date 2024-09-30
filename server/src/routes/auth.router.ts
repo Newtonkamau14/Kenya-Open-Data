d605d22541799d286c417716e81b5dde6b293b16
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-30 15:31:20 +0300

    feat: add check-session route

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -3,7 +3,8 @@ import { AuthController } from "../controllers/auth.controller";
 const router = Router();
 
 router.route("/signup").post(AuthController.signUp);
-router.route("/login").post(AuthController.login)
-router.route("/logout").post(AuthController.logout)
+router.route("/login").post(AuthController.login);
+router.route("/logout").post(AuthController.logout);
+router.route("/check-session").get(AuthController.checkSession)
 
 export default router;
