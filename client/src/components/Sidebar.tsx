004cc364064141148fdc20356888b16ed3df9b96
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-30 15:20:59 +0300

    style: format code

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,23 +1,28 @@
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import space_dashboard from "../assets/space_dashboard.svg";
 import list from "../assets/list.svg";
 import group from "../assets/group.svg";
 import credit from "../assets/credit.svg";
-
 
 function Sidebar() {
   const location = useLocation();
 
   return (
     <>
-      <aside className="bg-[#FFFFFF] border-1 shadow-lg w-1/6 min-h-screen flex flex-col justify-between">
+      <aside className="bg-[#FFFFFF] border-1 shadow-lg w-1/6 min-h-screen flex flex-col justify-between lg:w-1/5 md:w-1/4 sm:w-1/3">
         <ul className="flex flex-col items-start mt-3 ">
           <li
             className={`flex items-center w-full text-black rounded-md ${
-              location.pathname === "/app" ? "bg-[#f4f4f5]" : "hover:bg-[#f4f4f5]"
+              location.pathname === "/app"
+                ? "bg-[#f4f4f5]"
+                : "hover:bg-[#f4f4f5]"
             }`}
           >
-            <img src={space_dashboard} alt="dashboard" className="mx-2 inline-block" />
+            <img
+              src={space_dashboard}
+              alt="dashboard"
+              className="mx-2 inline-block"
+            />
             <Link to="/app" className="text-lg w-full py-2 px-4">
               Dashboard
             </Link>
@@ -68,4 +73,3 @@ function Sidebar() {
 }
 
 export default Sidebar;
-
