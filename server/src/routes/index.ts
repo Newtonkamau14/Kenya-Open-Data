9380f1d3117a1b4b8d2d5ac4fb8f2cf9f4ec0077
Newtonkamau14 <newtonkamau14@gmail.com>
2024-09-30 16:06:30 +0300

    feat: add api version

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import apiRouter from "./api.router";
 import apiKeyRouter from "./api-key.router";
 import authRouter from "./auth.router";
+const apiVersion = process.env.API_URL
 const router = Router();
 
-router.use("/", apiRouter);
+router.use(`${apiVersion}/`, apiRouter);
 router.use("/api-key",apiKeyRouter)
 router.use("/auth", authRouter);
 
