473c6ba892b13d7b578576d9a028df79ccf82417
Newtonkamau14 <newtonkamau14@gmail.com>
2024-10-02 14:16:31 +0300

    fix bug in requireAuth

--- server/src/middleware/middleware.ts
+++ server/src/middleware/middleware.ts
@@ -2,20 +2,22 @@ import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { ApiKeyRepository } from "../repository/api-key.repository";
 import { AuthRepository } from "../repository/auth.repository";
 
-
 const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
   const authRepository = new AuthRepository();
   const userId = req.session.userId;
 
+  if (!userId) {
+    return res
+      .status(401)
+      .json({ message: "Sign up or log in to your existing account" });
+  }
 
-  if (userId) {
-    const user = await authRepository.getUsername(userId)
-    req.session.username = user?.username
+  const user = await authRepository.getUsername(userId);
+
+  if (userId === user?.id) {
     next();
   } else {
-    res
-      .status(401)
-      .json({ message: " Sign up or log in to your existing account" });
+    res.status(403).json({ message: "You need to be logged in to view this." });
   }
 };
 
@@ -33,7 +35,7 @@ const authenticateKey = async (
 
   if (!API_KEY) {
     return res.status(401).json({
-      message: "Unauthorized",
+      message: "Please log in to get the API KEY.",
     });
   }
 
@@ -42,7 +44,7 @@ const authenticateKey = async (
   if (!user || user.apiKey !== API_KEY) {
     // Compare with the user's apiKey field
     return res.status(403).json({
-      message: "Forbidden",
+      message: "This action is not allowed for your account.",
     });
   }
 
