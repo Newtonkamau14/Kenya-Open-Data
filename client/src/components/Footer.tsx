89b52209650578c995238cbd341f7284f2b3fa1b
Newtonkamau14 <newtonkamau14@gmail.com>
2024-10-02 11:14:11 +0300

    fix: remove md:px-6 class

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
 
   const year = date.getFullYear();
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">
         © {year} Kenya Open Data. All rights reserved.
       </p>
